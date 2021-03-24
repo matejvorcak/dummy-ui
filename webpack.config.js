b79d3e5f711b3dda36cf78a213b293fac0010884
Matej Vorcak <matej.vorcak@doxxbet.sk>
2021-03-24 13:43:25 +0100

    added tailwind to project

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const { addTailwindPlugin } = require("@ngneat/tailwind");
+const tailwindConfig = require("./tailwind.config.js");
+
+module.exports = (config) => {
+  addTailwindPlugin({
+    webpackConfig: config,
+    tailwindConfig,
+    patchComponentsStyles: true
+  });
+  return config;
+};
